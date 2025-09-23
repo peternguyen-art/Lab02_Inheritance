2e83d5ccf351321ce8830cae26466c1486cb52d1
Peter Nguyen <nguye5he@mail.uc.edu>
2025-09-22 23:14:10 -0400

    added SalaryWorker and its test file

---
+++ src/SalaryWorker.java
@@ -0,0 +1,46 @@
+public class SalaryWorker extends Worker {
+
+    private double annualSalary;
+
+    /**
+     * Construction for SalaryWorker class
+     *
+     * @param IDNum         The worker's ID number.
+     * @param lastName      The worker's last name.
+     * @param firstName     The worker's first name.
+     * @param title         The worker's title (e.g., "Mr.", "Ms.").
+     * @param YOB           The worker's year of birth.
+     * @param hourlyPayRate The worker's hourly pay rate.
+     * @param annualSalary  The worker's annual salary.
+     */
+    public SalaryWorker(String IDNum, String lastName, String firstName, String title, int YOB, double hourlyPayRate, double annualSalary) {
+        super(IDNum, lastName, firstName, title, YOB, hourlyPayRate);
+        this.annualSalary = annualSalary;
+    }
+
+    @Override
+    public double calculateWeeklyPay(double hoursWorked) {
+        return this.annualSalary/ 52.0;
+    }
+
+    @Override
+    public void displayWeeklyPay(double hoursWorked) {
+        double weeklyPay = calculateWeeklyPay(hoursWorked);
+        System.out.printf("Weekly pay of a salary man is 1/52 of their annual rate $%.2f%n",weeklyPay);
+    }
+
+    @Override
+    public String toCSV() {
+        return super.toCSV() + ", " + this.annualSalary;
+    }
+
+    @Override
+    public String toXML() {
+        return super.toXML() + ", " + this.annualSalary;
+    }
+
+    @Override
+    public String toJSON(){
+        return super.toJSON() + ", " + this.annualSalary;
+    }
+}
